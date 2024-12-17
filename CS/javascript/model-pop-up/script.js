2d2080297cb9db76f18e3af4a68ac43431a289ca
Him-coder7 <125470302+Him-coder7@users.noreply.github.com>
2024-12-17 18:14:03 -0500

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,13 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("myModal");
+    const openModalButton = document.getElementById("");
+    const closeModalButton = document.getElementById("");
+    openModalButton.addEventListener("click", () => {
+        modal.style.display = "block";
+    });
+    closeModalButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+    window.addEventListener("click", (event) => {
+    });
+});
