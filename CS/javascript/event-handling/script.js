8d2a3fab39b8ad6cdd9da44bc84a3372bf1478e8
Him-coder7 <125470302+Him-coder7@users.noreply.github.com>
2024-12-17 18:05:40 -0500

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,5 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('').addEventListener('click', function() {
+    image = document.createElement('img');
+    image.src = "https://preview.redd.it/is-sonic-the-strongest-character-in-the-entire-franchise-v0-p8xpsdiul1kb1.jpg?width=640&crop=smart&auto=webp&s=2639728155acb72f2fde57fd47fff798088892a2";
+    document.body.appendChild(image);
+});
